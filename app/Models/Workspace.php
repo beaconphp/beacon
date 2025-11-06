60c8a7907ffffed4824ece79c567fa0639c874ee
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-06 19:10:34 +0100

    feat: add workspace model

---
+++ app/Models/Workspace.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+final class Workspace extends Model
+{
+    /** @return BelongsTo<User, $this> */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+}
---
+++ database/migrations/2025_11_06_091122_create_workspaces_table.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workspaces', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('avatar')->nullable();
+            $table->foreignId('author_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+};
