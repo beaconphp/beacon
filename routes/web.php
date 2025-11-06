2670df168e9fdf82b60c9fdcee81145261f5b477
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-06 19:10:11 +0100

    feat: prepare routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,22 @@
 
 declare(strict_types=1);
 
+use App\Livewire\Dashboard\Ticket\ListTickets;
+use App\Livewire\Dashboard\Workspace\CreateWorkspace;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::domain('{workspace}.'.config('app.domain'))->group(function () {
+    Route::get('');
+});
+
+Route::domain(config('app.domain'))->prefix('dashboard')->as('dashboard.')->group(function () {
+    Route::get('/login', App\Livewire\Dashboard\Auth\Login::class)->name('login');
+
+    Route::middleware(['auth.dashboard', 'can-access-dashboard'])->group(function () {
+        Route::middleware('has-workspace')->group(function () {
+            Route::get('/', ListTickets::class)->name('tickets.index');
+        });
+
+        Route::get('/workspace/new', CreateWorkspace::class)->name('workspaces.create');
+    });
 });
