9d9436ca76cdb9b349e69ba2c7ba65f097a15a88
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-06 19:10:25 +0100

    feat: add user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,48 +1,62 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+final class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    /** @return BelongsToMany<Workspace, $this> */
+    public function workspaces(): BelongsToMany
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->belongsToMany(Workspace::class);
+    }
+
+    /** @return HasMany<Workspace, $this> */
+    public function ownedWorkspaces(): HasMany
+    {
+        return $this->hasMany(Workspace::class, 'author_id');
+    }
+
+    /** @return BelongsTo<Workspace, $this> */
+    public function currentWorkspace(): BelongsTo
+    {
+        return $this->belongsTo(Workspace::class);
+    }
+
+    public function isCurrentWorkspace(Workspace $workspace): bool
+    {
+        return $workspace->id === $this->currentWorkspace?->id;
+    }
+
+    public function belongsToWorkspace(Workspace $workspace)
+    {
+        return $this->workspaces->contains(fn ($t): bool => $t->id === $workspace->id);
+    }
+
+    public function canAccessDashboard(): bool
+    {
+        return true;
     }
 }
