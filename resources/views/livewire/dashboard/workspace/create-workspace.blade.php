22b02b1eef0f0d2c522471bb3bd22e4986fa0fa1
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-06 21:44:26 +0100

    feat: add back to dashboard link to create workspace view

--- resources/views/livewire/dashboard/workspace/create-workspace.blade.php
+++ resources/views/livewire/dashboard/workspace/create-workspace.blade.php
@@ -1,9 +1,14 @@
-<div>
-    <flux:subheading class="text-center mb-6">Workspace is a place where you can organize your tickets</flux:subheading>
+<div class="space-y-6">
+    <flux:subheading class="text-center">Workspace is a place where you can organize your tickets</flux:subheading>
 
     <form wire:submit="save" class="flex flex-col gap-6">
         <flux:input label="Name" wire:model.live.debounce350ms="name" placeholder="Enter workspace name"/>
 
         <flux:button type="submit" variant="primary" class="w-full cursor-pointer">Create</flux:button>
     </form>
+
+    <flux:link variant="subtle" class="text-sm inline-flex items-center gap-2" href="{{ route('dashboard.tickets.index') }}">
+        <flux:icon.arrow-left class="size-4"/>
+        <span>Back to dashboard</span>
+    </flux:link>
 </div>
