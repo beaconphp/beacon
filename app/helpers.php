6a711e07eb1e363fa1ea6cabc5f3cbb16f66d037
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-07 16:27:10 +0100

    feat: add current workspace helper function

--- app/helpers.php
+++ app/helpers.php
@@ -3,10 +3,18 @@
 declare(strict_types=1);
 
 use App\Models\User;
+use App\Models\Workspace;
 
 if (! function_exists('current_user')) {
     function current_user(): ?User
     {
         return auth()->user();
+    }
+}
+
+if (! function_exists('current_workspace')) {
+    function current_workspace(): ?Workspace
+    {
+        return current_user()?->currentWorkspace;
     }
 }
