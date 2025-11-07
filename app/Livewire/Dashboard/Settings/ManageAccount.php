6c4fa0b86a778eca3e14fd614cb616c68e5764fe
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-07 16:26:29 +0100

    feat: ability to manage account

---
+++ app/Livewire/Dashboard/Settings/ManageAccount.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Livewire\Dashboard\Settings;
+
+use Illuminate\View\View;
+use Livewire\Component;
+
+final class ManageAccount extends Component
+{
+    public string $current_password = '';
+
+    public string $password = '';
+
+    public string $password_confirmation = '';
+
+    public function updatePassword(): void
+    {
+        $validated = $this->validate([
+            'current_password' => ['required', 'string', 'current_password'],
+            'password' => ['required', 'string', 'confirmed'],
+        ]);
+
+        auth()->user()->update([
+            'password' => $validated['password'],
+        ]);
+
+        $this->reset('current_password', 'password', 'password_confirmation');
+    }
+
+    public function render(): View
+    {
+        return view('livewire.dashboard.settings.manage-account')
+            ->layout('components.layouts.dashboard', ['title' => 'Settings']);
+    }
+}
---
+++ resources/views/livewire/dashboard/settings/manage-account.blade.php
@@ -0,0 +1,18 @@
+<x-dashboard.settings.template>
+    <div class="max-w-md space-y-12">
+        <form wire:submit="updatePassword" class="space-y-4">
+            <flux:input type="password" label="Current password" wire:model.live.debounce350ms="current_password" placeholder="Enter current password" viewable/>
+            <flux:input type="password" label="New password" wire:model.live.debounce350ms="password" placeholder="Enter new password" viewable/>
+            <flux:input type="password" label="Confirm new password" wire:model.live.debounce350ms="password_confirmation" placeholder="Confirm new password" viewable/>
+            <flux:button variant="primary" type="submit">Update password</flux:button>
+        </form>
+        <flux:separator/>
+        <div>
+            <flux:radio.group x-data x-model="$flux.appearance" label="Appearance" description="Select your theme" variant="segmented">
+                <flux:radio value="light" label="Light" icon="sun"/>
+                <flux:radio value="dark" label="Dark" icon="moon"/>
+                <flux:radio value="system" label="System" icon="computer-desktop"/>
+            </flux:radio.group>
+        </div>
+    </div>
+</x-dashboard.settings.template>
