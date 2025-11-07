a1c0a20ae1b5464819990ecac7fb7134ec52e03c
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-07 13:38:49 +0100

    feat: add users relation to workspace model

--- app/Models/Workspace.php
+++ app/Models/Workspace.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 final class Workspace extends Model
 {
@@ -13,5 +14,11 @@ final class Workspace extends Model
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class, 'author_id');
+    }
+
+    /** @return BelongsToMany<User, $this> */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
     }
 }
