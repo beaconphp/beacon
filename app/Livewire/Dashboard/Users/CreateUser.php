9bc1b8e2aad31546489d8811cdaf91cfec4e909b
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-10 13:01:40 +0100

    feat: add create user component

---
+++ app/Livewire/Dashboard/Users/CreateUser.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Livewire\Dashboard\Users;
+
+use App\Enums\UserRole;
+use App\Models\User;
+use Flux\Flux;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Illuminate\View\View;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+final class CreateUser extends Component
+{
+    public string $name = '';
+
+    public string $email = '';
+
+    public string $password = '';
+
+    public string $role = UserRole::CLIENT->value;
+
+    #[On('show-create-user-modal')]
+    public function openModal(): void
+    {
+        $this->reset();
+
+        $this->resetErrorBag();
+
+        Flux::modal('create-user')->show();
+    }
+
+    public function save(): void
+    {
+        $this->validate([
+            'name' => ['required', 'string', 'min:3', 'max:255'],
+            'email' => ['required', 'string', 'email', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8'],
+            'role' => ['required', 'string', Rule::in(UserRole::cases())],
+        ]);
+
+        $workspace = current_workspace();
+
+        $user = User::query()->create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+            'current_workspace_id' => $workspace->id,
+        ]);
+
+        $user->workspaces()->attach($workspace, ['role' => $this->role]);
+
+        $user->save();
+
+        Flux::modal('create-user')->close();
+
+        $this->dispatch('refresh-users-list');
+    }
+
+    public function render(): View
+    {
+        return view('livewire.dashboard.users.create-user');
+    }
+}
---
+++ resources/views/livewire/dashboard/users/create-user.blade.php
@@ -0,0 +1,26 @@
+<flux:modal name="create-user" class="md:min-w-md">
+    <div class="space-y-6">
+        <div class="flex items-center justify-between">
+            <flux:heading size="lg">New user</flux:heading>
+        </div>
+        <form wire:submit="save" class="space-y-6">
+            <div class="space-y-3">
+                <flux:input label="Name" wire:model="name" placeholder="Enter name"/>
+                <flux:input type="email" label="E-mail" wire:model="email" placeholder="Enter e-mail"/>
+                <flux:input type="password" label="Password" wire:model="password" placeholder="Enter password" viewable/>
+                <flux:radio.group wire:model="role" label="Role" variant="segmented">
+                    @foreach(\App\Enums\UserRole::availableForCreate() as $role)
+                        <flux:radio :label="$role->label()" :value="$role->value"/>
+                    @endforeach
+                </flux:radio.group>
+            </div>
+
+            <div class="flex items-center gap-3">
+                <flux:button type="submit" variant="primary" class="cursor-pointer">Create</flux:button>
+                <flux:modal.close>
+                    <flux:button variant="filled" class="cursor-pointer">Cancel</flux:button>
+                </flux:modal.close>
+            </div>
+        </form>
+    </div>
+</flux:modal>
