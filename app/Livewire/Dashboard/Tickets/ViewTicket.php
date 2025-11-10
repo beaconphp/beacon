36a8a686a81739bb8b23c796c759aa20ddc0d1e4
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-10 13:01:19 +0100

    feat: add view ticket component (wip)

---
+++ app/Livewire/Dashboard/Tickets/ViewTicket.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Livewire\Dashboard\Tickets;
+
+use App\Models\Ticket;
+use App\Models\User;
+use Flux\Flux;
+use Illuminate\View\View;
+use Livewire\Attributes\Locked;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+final class ViewTicket extends Component
+{
+    #[Locked]
+    public Ticket $ticket;
+
+    public string $subject = '';
+
+    public string $status = '';
+
+    public string $priority = '';
+
+    public null|int|string $assigned_to = null;
+
+    #[On('show-view-ticket-modal')]
+    public function setTicket(Ticket $ticket): void
+    {
+        $this->ticket = $ticket;
+
+        $this->fill($ticket);
+
+        Flux::modal('view-ticket')->show();
+    }
+
+    public function updatedStatus(string $value): void
+    {
+        $this->ticket->update([
+            'status' => $value,
+        ]);
+
+        $this->dispatch('refresh-ticket-list');
+    }
+
+    public function updatedPriority(string $value): void
+    {
+        $this->ticket->update([
+            'priority' => $value,
+        ]);
+
+        $this->dispatch('refresh-ticket-list');
+    }
+
+    public function assignUser(int $value): void
+    {
+        $assignee = User::query()->find($value);
+
+        if (! $assignee->belongsToWorkspace(current_workspace())) {
+            return;
+        }
+
+        if ($this->ticket->assignee && $assignee->id === $this->ticket->assignee->id) {
+            $this->ticket->assignee()->disassociate();
+        } else {
+            $this->ticket->assignee()->associate($value);
+        }
+
+        $this->ticket->save();
+
+        $this->dispatch('refresh-ticket-list');
+    }
+
+    public function deleteDescription(): void
+    {
+        $this->ticket->update([
+            'description' => null,
+        ]);
+    }
+
+    public function render(): View
+    {
+        return view('livewire.dashboard.tickets.view-ticket');
+    }
+}
---
+++ resources/views/livewire/dashboard/tickets/view-ticket.blade.php
@@ -0,0 +1,94 @@
+<flux:modal name="view-ticket" variant="flyout" class="w-full md:w-1/2 md:min-w-[300px] xl:max-w-[800px]">
+    @if($ticket)
+        <div class="space-y-6">
+            <flux:heading size="lg">{{ $ticket->subject }}</flux:heading>
+            <div class="flex flex-col gap-2">
+                <div class="flex items-center h-10 gap-5 border-b border-zinc-200 dark:border-zinc-700">
+                    <flux:label class="md:w-[120px]">Status</flux:label>
+                    <div class="flex-1" wire:key="ticket-{{$ticket->id}}-status-{{ $ticket->status }}">
+                        <flux:dropdown>
+                            <button>
+                                <flux:badge size="sm" :color="$ticket->status->color()" class="cursor-pointer">{{ $ticket->status->label() }}</flux:badge>
+                            </button>
+
+                            <flux:menu>
+                                <flux:menu.radio.group wire:model.live.debounce250ms="status">
+                                    @foreach(\App\Enums\TicketStatus::cases() as $s)
+                                        <flux:menu.radio value="{{ $s->value }}">{{ $s->label() }}</flux:menu.radio>
+                                    @endforeach
+                                </flux:menu.radio.group>
+                            </flux:menu>
+                        </flux:dropdown>
+                    </div>
+                </div>
+                <div class="flex items-center h-10 gap-5 border-b border-zinc-200 dark:border-zinc-700">
+                    <flux:label class="md:w-[120px]">Priority</flux:label>
+                    <div class="flex-1">
+                        <flux:dropdown>
+                            <button class="cursor-pointer flex items-center gap-2">
+                                <flux:icon.exclamation-triangle variant="solid" @class([
+                                    'size-5',
+                                    'text-zinc-500' => $ticket->priority === \App\Enums\TicketPriority::NORMAL,
+                                    'text-lime-500' => $ticket->priority === \App\Enums\TicketPriority::LOW,
+                                    'text-yellow-500' => $ticket->priority === \App\Enums\TicketPriority::MEDIUM,
+                                    'text-rose-400' => $ticket->priority === \App\Enums\TicketPriority::HIGH,
+                                    'text-red-500' => $ticket->priority === \App\Enums\TicketPriority::CRITICAL,
+                                ])/>
+                                <span class="text-sm mb-0.5">{{ $ticket->priority->label() }}</span>
+                            </button>
+
+                            <flux:menu>
+                                <flux:menu.radio.group wire:model.live.debounce250ms="priority">
+                                    @foreach(\App\Enums\TicketPriority::cases() as $p)
+                                        <flux:menu.radio value="{{ $p->value }}">{{ $p->label() }}</flux:menu.radio>
+                                    @endforeach
+                                </flux:menu.radio.group>
+                            </flux:menu>
+                        </flux:dropdown>
+                    </div>
+                </div>
+                <div class="flex items-center h-10 gap-5 border-b border-zinc-200 dark:border-zinc-700">
+                    <flux:label class="md:w-[120px]">Assignee</flux:label>
+                    <div class="flex-1" wire:key="ticket-{{$ticket->id}}-assignee-{{ $ticket->assigned_to }}">
+                        <flux:dropdown>
+                            <button class="cursor-pointer flex items-center gap-2">
+                                @if($ticket->assignee)
+                                    <flux:avatar :name="$ticket->assignee->name" size="xs" circle/>
+                                    <span class="text-sm">{{ $ticket->assignee->name }}</span>
+                                @else
+                                    <span class="text-xs italic text-zinc-400">none</span>
+                                @endif
+                            </button>
+
+                            <flux:menu>
+                                @foreach(current_workspace()->users as $u)
+                                    <flux:menu.item wire:click="assignUser({{ $u->id }})" class="cursor-pointer">{{ $u->name }}</flux:menu.item>
+                                @endforeach
+                            </flux:menu>
+                        </flux:dropdown>
+                    </div>
+                </div>
+            </div>
+            <div class="group">
+                <div class="flex items-center mb-2">
+                    <flux:label>Ticket description</flux:label>
+                    @if($ticket->description)
+                        <flux:dropdown class="ms-auto">
+                            <flux:button icon="ellipsis-horizontal" size="xs"/>
+
+                            <flux:menu>
+                                <flux:menu.item icon="pencil">Edit description</flux:menu.item>
+                                <flux:menu.item wire:click="deleteDescription" variant="danger" icon="trash">Delete description</flux:menu.item>
+                            </flux:menu>
+                        </flux:dropdown>
+                    @endif
+                </div>
+                @if($ticket->description)
+                    <flux:text size="lg">{!! html_entity_decode($ticket->description) !!}</flux:text>
+                @else
+                    <flux:button>Add ticket description</flux:button>
+                @endif
+            </div>
+        </div>
+    @endif
+</flux:modal>
