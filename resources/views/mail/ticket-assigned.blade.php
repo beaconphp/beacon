8efdb046956818cb002d41673895fe06f1267ef1
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-23 22:34:53 +0100

    feat: add ticket details to mail

--- resources/views/mail/ticket-assigned.blade.php
+++ resources/views/mail/ticket-assigned.blade.php
@@ -1,5 +1,8 @@
 <x-layouts.mail :message="$message">
     <p style="color: #2a2627;">New ticket <b>"{{ $ticket->subject }}"</b> has been assigned to you in <b>{{ $ticket->workspace->name }}</b> workspace.</p>
-    <p style="margin-top: 30px; color: #2a2627;">Here is the ticket description:</p>
-    <div style="color: #2a2627;">{{ $ticket->description }}</div>
+    <p style="margin-top: 30px; color: #2a2627;">Here are the ticket details:</p>
+    <p style="margin-top: 10px; color: #2a2627;">Status: <b>{{ $ticket->status?->label() }}</b></p>
+    <p style="margin-top: 10px; color: #2a2627;">Priority: <b>{{ $ticket->priority?->label() }}</b></p>
+    <p style="margin-top: 30px; color: #2a2627;">And here is the ticket description:</p>
+    <div style="margin-top: 10px; color: #2a2627; font-size: 18px">{{ $ticket->description }}</div>
 </x-layouts.mail>
