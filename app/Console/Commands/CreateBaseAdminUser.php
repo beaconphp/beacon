b099e77361375fbaf05f91d1f00d59a75bd5e44d
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-23 19:06:48 +0100

    feat: add admin make command

---
+++ app/Console/Commands/CreateBaseAdminUser.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+final class CreateBaseAdminUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:admin-user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Makes base admin user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        User::query()
+            ->create([
+                'name' => 'Admin',
+                'email' => 'admin@beacon.com',
+                'password' => bcrypt('admin'),
+            ]);
+    }
+}
