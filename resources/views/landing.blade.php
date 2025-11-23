a805360c4b75b631a69b3120bd01d35848ec86ef
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-23 19:25:24 +0100

    feat: add landing page (wip)

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.base title="Beacon">
+    <x-slot:body class="w-screen flex justify-center items-center h-screen text-zinc-900 bg-zinc-50 dark:text-zinc-50 dark:bg-zinc-900">
+        <x-logo class="h-10"/>
+    </x-slot:body>
+</x-layouts.base>
--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,23 @@ Route::domain('{workspace}.'.config('app.domain'))->group(function () {
     Route::get('');
 });
 
-Route::domain(config('app.domain'))->prefix('dashboard')->as('dashboard.')->group(function () {
-    Route::get('/login', App\Livewire\Dashboard\Auth\Login::class)->name('login');
+Route::domain(config('app.domain'))->group(function () {
+    Route::view('/', 'landing');
 
-    Route::middleware(['auth.dashboard', 'can-access-dashboard'])->group(function () {
-        Route::middleware('has-workspace')->group(function () {
-            Route::get('/', ListTickets::class)->name('tickets.index');
-            Route::get('/users', ListUsers::class)->name('users.index');
-            Route::get('/user/new', CreateUser::class)->name('users.create');
-            Route::get('/settings/workspace', ManageWorkspace::class)->name('settings.workspace');
-            Route::get('/settings/account', ManageAccount::class)->name('settings.account');
+    Route::prefix('dashboard')->as('dashboard.')->group(function () {
+        Route::get('/login', App\Livewire\Dashboard\Auth\Login::class)->name('login');
+
+        Route::middleware(['auth.dashboard', 'can-access-dashboard'])->group(function () {
+            Route::middleware('has-workspace')->group(function () {
+                Route::get('/', ListTickets::class)->name('tickets.index');
+                Route::get('/users', ListUsers::class)->name('users.index');
+                Route::get('/user/new', CreateUser::class)->name('users.create');
+                Route::get('/settings/workspace', ManageWorkspace::class)->name('settings.workspace');
+                Route::get('/settings/account', ManageAccount::class)->name('settings.account');
+            });
+
+            Route::get('/workspace/new', CreateWorkspace::class)->name('workspaces.create');
         });
-
-        Route::get('/workspace/new', CreateWorkspace::class)->name('workspaces.create');
     });
 });
 
