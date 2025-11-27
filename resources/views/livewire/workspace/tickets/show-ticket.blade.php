ca66c64d1317534a75ecf2d41b2b2264be11026d
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-27 21:12:44 +0100

    feat: add description to ticket view

--- resources/views/livewire/workspace/tickets/show-ticket.blade.php
+++ resources/views/livewire/workspace/tickets/show-ticket.blade.php
@@ -1,3 +1,7 @@
 <div class="max-w-lg mx-auto space-y-6">
-    <flux:heading size="xl">{{ $ticket->subject }}</flux:heading>
+    <div class="space-y-6">
+        <flux:heading size="xl">{{ $ticket->subject }}</flux:heading>
+        <flux:text size="xl">{!! html_entity_decode($ticket->description) !!}</flux:text>
+    </div>
+    <flux:separator/>
 </div>
