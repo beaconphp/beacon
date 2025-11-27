d1c3830bb020c78d9c49f713c4741f9b18facd82
Pavel Skrbel <fgocsgo@gmail.com>
2025-11-27 16:46:32 +0100

    refactor: use env variables in config

--- config/beacon.php
+++ config/beacon.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'landing_page' => true,
-    'subdomain_workspaces' => true,
+    'landing_page' => env('BEACON_LANDING_PAGE', true),
+    'subdomain_workspaces' => env('BEACON_SUBDOMAIN_WORKSPACES', true),
 ];
